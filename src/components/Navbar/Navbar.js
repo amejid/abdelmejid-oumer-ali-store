6293ea08d7826b372456e38e198c68cb8db33c77
amejid <abdelmejid89@gmail.com>
2022-11-12 22:42:16 +0300

    Switch currency from store

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,46 @@
 import { PureComponent } from 'react';
+import { gql } from '@apollo/client';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 import styles from './Navbar.module.css';
 import logo from './logo.svg';
 import cart from './cart.svg';
+import arrowDown from './arrow-down.svg';
+import apolloClient from '../../apolloClient';
+import { setCurrency } from '../../store';
+
+const getCurrenciesQuery = gql`
+query {
+  currencies{
+    label
+    symbol
+  }
+}
+`;
 
 class Navbar extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      currencies: [],
+    };
+  }
+
+  componentDidMount() {
+    apolloClient.query({
+      query: getCurrenciesQuery,
+    }).then((res) => {
+      this.setState({
+        isLoaded: true,
+        currencies: res.data.currencies,
+      });
+    });
+  }
+
   render() {
-    const { items } = this.props;
+    const { items, currency, setCurrency } = this.props;
+    const { isLoaded, currencies } = this.state;
     return (
       <header className={styles.header}>
         <nav>
@@ -17,7 +51,22 @@ class Navbar extends PureComponent {
         <div>
           <img src={logo} alt="Cart Logo" />
         </div>
-        <div>
+        <div className={styles['cart-container']}>
+          <div className={styles.dropdown}>
+            <button className={styles.dropbtn} type="button">
+              <span>{currency.symbol}</span>
+              <img src={arrowDown} alt="Arrow Down" />
+            </button>
+            <div className={styles['dropdown-content']}>
+              {isLoaded && currencies.map((currency, index) => (
+                <button key={currency.label} type="button" onClick={() => setCurrency({ label: currency.label, symbol: currency.symbol, value: index })}>
+                  <span>{currency.symbol}</span>
+                  &nbsp;
+                  {currency.label}
+                </button>
+              ))}
+            </div>
+          </div>
           <img src={cart} alt="Cart Icon" />
         </div>
       </header>
@@ -25,4 +74,8 @@ class Navbar extends PureComponent {
   }
 }
 
-export default Navbar;
+const mapStateToProps = (state) => ({
+  currency: state.currency,
+});
+
+export default connect(mapStateToProps, { setCurrency })(Navbar);
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,17 +1,22 @@
 import { PureComponent } from 'react';
+import { connect } from 'react-redux';
 import styles from './ProductItem.module.css';
 
 class ProductItem extends PureComponent {
   render() {
-    const { product } = this.props;
+    const { product, currency } = this.props;
     return (
       <li className={styles.productItem}>
         <img src={product.gallery[0]} alt="Product" className={styles['product-img']} />
         <p className={styles['product-name']}>{product.name}</p>
-        <p className={styles['product-price']}>{`${product.prices[0].currency.symbol} ${product.prices[0].amount}`}</p>
+        <p className={styles['product-price']}>{`${product.prices[currency.value].currency.symbol} ${product.prices[currency.value].amount}`}</p>
       </li>
     );
   }
 }
 
-export default ProductItem;
+const mapStateToProps = (state) => ({
+  currency: state.currency,
+});
+
+export default connect(mapStateToProps)(ProductItem);
