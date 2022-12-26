a2d6d896c6d15d9426e2cdfc23a64dbc0d436660
amejid <abdelmejid89@gmail.com>
2022-12-26 18:54:37 +0300

    Add Out-of-Stock functionality and styles

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -10,12 +10,19 @@ class ProductItem extends PureComponent {
     const { product, currency, addProduct } = this.props;
     return (
       <li className={styles['product-item']}>
+        {!product.inStock && (
+          <Link to={`${product.id}`} className={styles['out-stock']}>
+            <span className={styles['out-stock-text']}>Out of stock</span>
+          </Link>
+        ) }
         <Link to={`${product.id}`} className={styles['product-link']}>
-          <img src={product.gallery[0]} alt="Product" className={styles['product-img']} />
+          <div className={styles['product-img-container']}>
+            <img src={product.gallery[0]} alt="Product" className={styles['product-img']} />
+          </div>
           <p className={styles['product-name']}>{`${product.brand} ${product.name}`}</p>
-          <p className={styles['product-price']}>{`${product.prices[currency.value].currency.symbol} ${product.prices[currency.value].amount}`}</p>
+          <p className={styles['product-price']}>{`${product.prices[currency.value].currency.symbol}${product.prices[currency.value].amount}`}</p>
         </Link>
-        { product.attributes.length === 0 && (
+        {product.inStock && product.attributes.length === 0 && (
         <button className={styles.cta} type="button" onClick={() => addProduct(product)}>
           <img src={cartImg} alt="Cart icon" />
         </button>
